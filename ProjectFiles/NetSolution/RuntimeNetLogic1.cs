8128af88f10d4ab0cc6fbcd8b9f4f8fc83c2fd29
xavierflorensa <xavier.florensa@yahoo.com>
2024-01-16 20:21:48 +0100

    First version with only one Holding register at memory position 0

---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,41 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.NetLogic;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using EasyModbus;
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+     PeriodicTask myTask;
+    ModbusClient myModbusClient;
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+        myModbusClient = new ModbusClient("127.0.0.1",502);
+        myModbusClient.Connect();
+        int[] readHoldingRegisters = myModbusClient.ReadHoldingRegisters(0,2);
+        Log.Info(readHoldingRegisters[0].ToString());
+        myTask = new PeriodicTask(MB_Server_Task, 100, LogicObject);
+        myTask.Start();
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+        myTask.Dispose();
+    }
+    private void MB_Server_Task()
+    {
+        int[] readHoldingRegisters = myModbusClient.ReadHoldingRegisters(0,2);
+        Log.Info(readHoldingRegisters[0].ToString());
+        Owner.Get<CircularGauge>("CircularGauge1").Value=readHoldingRegisters[0];
+    }
+}
